997656b022e6aa2276222f8f7e45364b794e8f21
Николай Меркульев <55264259+Nikolai7654@users.noreply.github.com>
2020-06-18 15:55:17 +0300

    лаб 3 и лаб 7

---
+++ Bot1/ChatBot.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Bot1
+{
+    class ChatBot
+    {
+        string s,
+      path,
+          userAnswer;
+        List<string> sempls = new List<string>();
+        bool flag = true;
+        public event Action<string> GetStr;
+
+        public ChatBot(string pat)
+        {
+            path = pat;
+            try
+            {
+                sempls.AddRange(File.ReadAllLines(path));
+            }
+            catch { }
+            GetStr += ChatBot_GetStr;
+
+
+        }
+        private string Answer(string qw)
+        {
+            string tr = "():^^*!?",
+              ans = string.Empty;
+            qw = qw.ToLower();
+            qw = Trim(qw, tr.ToCharArray());
+            for (int i = 0; i < sempls.Count; i += 2)
+            {
+                if (qw == sempls[i])
+                {
+                    ans = sempls[i + 1];
+                    break;
+                }
+            }
+            return ans;
+        }
+
+        private void Teach()
+        {
+            sempls.Add(s);
+            sempls.Add(userAnswer);
+            File.WriteAllLines(path, sempls.ToArray());
+        }
+        public void Ans(string qw)
+        {
+            if (flag)
+            {
+                s = qw;
+                string ans = Answer(qw);
+
+                if (ans == string.Empty)
+                {
+                    flag = false;
+                    GetStr("\n Введите ответ: ");
+                }
+                else
+                    GetStr("\n Введите ответ: " + ans + "\n Вфш вопрос: ");
+            }
+            else
+            {
+                flag = true;
+                userAnswer = qw;
+                Teach();
+                GetStr("\n Ваш вопрос: ");
+            }
+        }
+        string Trim(string str, char[] chars)
+        {
+            string strA = str;
+            for (int i = 0; i < chars.Length; i++)
+            {
+                strA = strA.Replace(char.ToString(chars[i]), "");
+            }
+            return strA;
+
+        }
+        void ChatBot_GetStr(string obj)
+        {
+
+        }
+
+
+    }
+}
+
---
+++ Bot1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Bot1
+{
+    class Program
+    {
+        static void Bot1(string obj)
+        {
+            Console.WriteLine(obj);
+        }
+        static void Main(string[] args)
+        {
+           ChatBot bot =new ChatBot(@"C:\Users\Николай\Desktop\otr.txt");
+            bot.GetStr += Bot1;
+            while(true)
+            {
+                string s = Console.ReadLine();
+                bot.Ans(s);
+            }
+        }
+    }
+}
